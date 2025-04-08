15b3f41f08a34ff29fb4b5adc24139c8d2f68b75
yukyan <yukyan@pepabo.com>
2025-04-09 00:33:14 +0900

    delete memo

--- main.go
+++ main.go
@@ -249,6 +249,3 @@ func fetchAllItems(client *github.Client, username string, dateRange model.DateR
 
 	return allItems, nil
 }
-
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
